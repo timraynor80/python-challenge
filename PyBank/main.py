f0bf9c52f7a2d62d293fa5236ed6055d01df7a5b
TIm Raynor <55665132+timraynor80@users.noreply.github.com>
2019-11-01 10:49:54 -0500

    201911011049

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,8 @@
 import os
 import csv
 from statistics import mean
+import math
+
 filename = 'budget_data.csv'
 
 date = []
@@ -11,22 +13,30 @@ tempTotalRow = 0
 currentTotalRow = 0
 TotalProfits = 0
 
+
 with open(filename, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+
     for row in csvreader:
         maxTotal = max(row[1])
         currentTotalRow = row[1]
+        
         if int(tempTotalRow) > 0:
             difference.append(int(currentTotalRow) - int(tempTotalRow))
+
         date.append(row[0])
-        totals.append(row[1])
+        totals.append(int(row[1]))
         tempTotalRow = currentTotalRow
         TotalProfits = TotalProfits + int(currentTotalRow)
-    print(max(totals))
+
     totalAvg = mean(difference)
+    GreatestIncreaseMonthIndex = totals.index(max(totals))
+    GreatestDecreaseMonthIndex = totals.index(min(totals))
 
 print("Total Months: " + str(len(totals)))
 print("Total Profits/Losses: " + str(TotalProfits))
 print("Average Change: " + str(totalAvg))
+print("Greatest Increase in Profits: " + date[GreatestIncreaseMonthIndex] + " (" + str(max(totals)) + ")")
+print("Greatest Decrease in Profits: " + date[GreatestDecreaseMonthIndex] + " (" + str(min(totals)) + ")")
 #need to convert average change to be currency
