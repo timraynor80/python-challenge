94e53bc50b4bac067b4a0002e549e829c9d24fdc
TIm Raynor <55665132+timraynor80@users.noreply.github.com>
2019-11-01 15:51:17 -0500

    201911011551

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,42 +1,38 @@
 import os
 import csv
-from statistics import mean
-import math
 
-filename = 'budget_data.csv'
+BudgetData = 'budget_data.csv'
+Date = []
+ProfLoss = []
+MonthlyDifference = []
+CurMoValue = 0
+Difference = []
 
-date = []
-totals = []
-difference = []
-Counter = 0
-tempTotalRow = 0
-currentTotalRow = 0
-TotalProfits = 0
-
-
-with open(filename, newline='') as csvfile:
+with open(BudgetData, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
     for row in csvreader:
-        maxTotal = max(row[1])
-        currentTotalRow = row[1]
-        
-        if int(tempTotalRow) > 0:
-            difference.append(int(currentTotalRow) - int(tempTotalRow))
+        Date.append(row[0])
+        ProfLoss.append(int(row[1]))
 
-        date.append(row[0])
-        totals.append(int(row[1]))
-        tempTotalRow = currentTotalRow
-        TotalProfits = TotalProfits + int(currentTotalRow)
+        if CurMoValue == 0:
+            CurMoValue = int(row[1])
+            PrevMoValue = CurMoValue   
+        CurMoValue = int(row[1])
+        Difference.append(int(CurMoValue) - int(PrevMoValue))
+        PrevMoValue = CurMoValue
 
-    totalAvg = mean(difference)
-    GreatestIncreaseMonthIndex = totals.index(max(totals))
-    GreatestDecreaseMonthIndex = totals.index(min(totals))
+    print("Total Months: " + str(len(Date)))
 
-print("Total Months: " + str(len(totals)))
-print("Total Profits/Losses: " + str(TotalProfits))
-print("Average Change: " + str(totalAvg))
-print("Greatest Increase in Profits: " + date[GreatestIncreaseMonthIndex] + " (" + str(max(totals)) + ")")
-print("Greatest Decrease in Profits: " + date[GreatestDecreaseMonthIndex] + " (" + str(min(totals)) + ")")
-#need to convert average change to be currency
+    TotalProfits = sum(ProfLoss)
+    print("Total Profits/Losses: " + str(TotalProfits))
+
+    AverageChange = int(sum(Difference)) / (int(len(Difference)) - 1)
+    print("Average Change: " + str(AverageChange))
+
+    GreatestMonthIncreaseIndex = Difference.index(max(Difference))
+    print("Greatest Increase in Profits: " + Date[GreatestMonthIncreaseIndex] + " (" + str(Difference[GreatestMonthIncreaseIndex]) + ")")
+  
+    GreatestMonthDecreaseIndex = Difference.index(min(Difference))
+    print("Greatest Decrease in Profits: " + Date[GreatestMonthDecreaseIndex] + " (" + str(Difference[GreatestMonthDecreaseIndex]) + ")")
