d00de48e58e30b33415288f99deb9c0627ee199e
TIm Raynor <55665132+timraynor80@users.noreply.github.com>
2019-11-02 09:55:46 -0500

    201911020955

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,8 @@ ElectionData = 'election_data.csv'
 VoterID  = []
 Candidate = []
 VoteFor = []
+
+#create and open output.txt file
 output = open("output.txt", "w")
 
 with open(ElectionData, newline='') as csvfile:
@@ -17,10 +19,14 @@ with open(ElectionData, newline='') as csvfile:
     print("------------------------------")
    
     for row in csvreader:
+        #update VoterID to use later in TotalVotes
         VoterID.append(row[0])
+        #Calculate TotalVotes by using len(list) on VoterID for a count of values in the VoterID list. 
         TotalVotes = len(VoterID)
+        #update VoteFor with list.append(row[i]) to get a list of all the Votes cast.
         VoteFor.append(row[2])
 
+        #loop through the row of votes cast and add unique values to Candidate list. 
         if row[2] not in Candidate:
             Candidate.append(row[2])
   
@@ -29,17 +35,23 @@ with open(ElectionData, newline='') as csvfile:
     print("Total Votes: " + str(TotalVotes))
     print("------------------------------")
 
+    #for each unique value in Candidate:
     for CandidateName in Candidate:
+        #calculate how many votes each candidate received by using list.count(str(i))
         VoteCount = VoteFor.count(str(CandidateName))
+        #calculate the percentage of TotalVotes each candidate received by dividing VoteCount by TotalVotes
         VotePercentage = VoteCount / TotalVotes
+        #set VotePercentage to proper decimal format
         VotePercentage = '{0:.3f}%'.format(VotePercentage*100)
         print(f'{CandidateName}: {VotePercentage} ({VoteCount})', file=output)
         print(f'{CandidateName}: {VotePercentage} ({VoteCount})')
-        curr_frequency = VoteFor.count(CandidateName)
         
     print("------------------------------", file=output)
     print("------------------------------")
+
+    #determine Winner by defining Winner(VoteFor)
     def Winner(VoteFor):
+        #returning max(set(list)), key = list.count
         return max(set(VoteFor), key = VoteFor.count)
     print(f'Winner: {Winner(VoteFor)}', file=output)
     print("------------------------------", file=output)
