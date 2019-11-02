c40b673aa2a2a66b66ae3ed944dff4ba574ff797
TIm Raynor <55665132+timraynor80@users.noreply.github.com>
2019-11-01 23:39:42 -0500

    201911012339

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,15 +5,17 @@ ElectionData = 'election_data.csv'
 VoterID  = []
 Candidate = []
 VoteFor = []
+output = open("output.txt", "w")
 
 with open(ElectionData, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-        # print(csv_header)
-
+    
+    print("Election Results", file=output)
+    print("------------------------------", file=output)
     print("Election Results")
     print("------------------------------")
-
+   
     for row in csvreader:
         VoterID.append(row[0])
         TotalVotes = len(VoterID)
@@ -22,12 +24,24 @@ with open(ElectionData, newline='') as csvfile:
         if row[2] not in Candidate:
             Candidate.append(row[2])
   
+    print("Total Votes: " + str(TotalVotes), file=output)
+    print("------------------------------", file=output)
     print("Total Votes: " + str(TotalVotes))
     print("------------------------------")
 
     for CandidateName in Candidate:
-        # print(VoteFor.count(str(CandidateName)))
-        print(f'{CandidateName}: ({VoteFor.count(str(CandidateName))})')
+        VoteCount = VoteFor.count(str(CandidateName))
+        VotePercentage = VoteCount / TotalVotes
+        VotePercentage = '{0:.3f}%'.format(VotePercentage*100)
+        print(f'{CandidateName}: {VotePercentage} ({VoteCount})', file=output)
+        print(f'{CandidateName}: {VotePercentage} ({VoteCount})')
+        curr_frequency = VoteFor.count(CandidateName)
+        
+    print("------------------------------", file=output)
     print("------------------------------")
-    print("Winner :")
+    def Winner(VoteFor):
+        return max(set(VoteFor), key = VoteFor.count)
+    print(f'Winner: {Winner(VoteFor)}', file=output)
+    print("------------------------------", file=output)
+    print(f'Winner: {Winner(VoteFor)}')
     print("------------------------------")
